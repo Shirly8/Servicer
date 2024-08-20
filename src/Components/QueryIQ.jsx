a64eac03257c7268bf4bf689257209bfdbe980cd
“Shirley” <ShirleyHuang7@hotmail.com>
2024-08-19 23:57:52 -0400

    Added QueryIQ Descriptions

--- src/Components/QueryIQ.jsx
+++ src/Components/QueryIQ.jsx
@@ -7,9 +7,21 @@ function QueryIQ() {
 
   return (
     <>
-      <div>
-        <h1>BYE</h1>
+      <div className = "full">
+      <h1 style = {{fontSize: "45px", textAlign: "center"}}> QueryIQ</h1>
+      <p style= {{paddingLeft: "5%", paddingRight:"5%", fontSize: "13px"}}>
+      QueryIQ, a state-of-the-art Retrieval-Augmented Generation (RAG) that provides businesses with an AI-powered chatbot that understands and responds to customer inquiries with remarkable accuracy.
+      QueryIQ is built with a deep understanding of your business-specific knowledge base, such as your company's unique products, services, documents and industry-specific handbook. 
+      This specialization delivers responses with not only with contextual precision, but highly relevant to your business needs.
+      </p>
 
+     <div className = "chat">
+
+     <input className = "inputText"></input>
+     <div className = "send-icon"></div>
+
+
+    </div> 
 
       </div>
   
